6d401dc9ee796aeed6d194e0ca64f1cfeffbbe58
Erion Schlenger de Paiva Maia <erion@riseio.com>
2026-05-16 19:57:58 -0300

    test: valida conexao jdbc com postgres.

--- sistema-contas-transferencias/src/main/java/br/com/erionmaia/util/ConnectionFactory.java
+++ sistema-contas-transferencias/src/main/java/br/com/erionmaia/util/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/java_web_db";
+    private static final String URL = "jdbc:postgresql://localhost:5433/java_web_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
 
