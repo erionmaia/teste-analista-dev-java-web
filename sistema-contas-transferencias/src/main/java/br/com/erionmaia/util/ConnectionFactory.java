f73d46beb0ce7b5bffb514042ee9c06a265406e4
Erion Schlenger de Paiva Maia <erion@riseio.com>
2026-05-16 19:00:22 -0300

    feat: adiciona connection factory jdbc com postgres

---
+++ sistema-contas-transferencias/src/main/java/br/com/erionmaia/util/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.erionmaia.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/java_web_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private ConnectionFactory() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
