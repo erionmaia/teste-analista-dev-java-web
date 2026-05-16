5062e4a8b7dd6ef84eebdae35de1a5f14f40ad9a
Erion Schlenger de Paiva Maia <erion@riseio.com>
2026-05-16 19:55:15 -0300

    fix: alteracao da porta utilizada pelo postgres.

--- sistema-contas-transferencias/src/main/java/br/com/erionmaia/Main.java
+++ sistema-contas-transferencias/src/main/java/br/com/erionmaia/Main.java
@@ -1,17 +1,36 @@
 package br.com.erionmaia;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.erionmaia.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String sql = "SELECT * FROM conta";
+
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)
+        )
+        {
+            if (connection != null) {
+                System.out.println("Conexão com PostgreSQL realizada com sucesso!");
+
+                while(resultSet.next()) {
+                    System.out.println("ID: " + resultSet.getInt("id"));
+                    System.out.println("Titular: " + resultSet.getString("nome_titular"));
+                    System.out.println("Saldo: " + resultSet.getBigDecimal("saldo"));
+                    System.out.println("--------------------------");
+                }
+            } else {
+                System.out.println("Falha ao conectar.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
