baef75efc6ce68a60bf7cc64cb92da601d079076
Erion Schlenger de Paiva Maia <erion@riseio.com>
2026-05-17 19:08:26 -0300

    feat: adiciona consulta paginada de movimentacoes.

--- sistema-contas-transferencias/src/main/java/br/com/erionmaia/dao/MovimentacaoDAO.java
+++ sistema-contas-transferencias/src/main/java/br/com/erionmaia/dao/MovimentacaoDAO.java
@@ -1,10 +1,11 @@
 package br.com.erionmaia.dao;
 
 import br.com.erionmaia.model.Movimentacao;
+import br.com.erionmaia.util.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovimentacaoDAO {
 
@@ -24,5 +25,56 @@ public class MovimentacaoDAO {
 
             stmt.executeUpdate();
         }
+    }
+
+    public List<Movimentacao> listarPorConta(Integer contaId, int pagina, int tamanho) throws SQLException {
+
+        String sql =
+                "SELECT * FROM movimentacao " +
+                "WHERE (? IS NULL OR conta_origem_id = ? OR conta_destino_id = ?) " +
+                "ORDER BY data_hora DESC " +
+                "LIMIT ? OFFSET ?";
+
+        List<Movimentacao> movimentacoes = new ArrayList<>();
+
+        int offset = (pagina - 1) * tamanho;
+
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ){
+            if (contaId != null) {
+                stmt.setNull(1, Types.INTEGER);
+                stmt.setNull(2, Types.INTEGER);
+                stmt.setNull(3, Types.INTEGER);
+            } else {
+                stmt.setInt(1, contaId);
+                stmt.setInt(2, contaId);
+                stmt.setInt(3, contaId);
+            }
+
+            stmt.setInt(4, tamanho);
+            stmt.setInt(5, offset);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    movimentacoes.add(mapearMovimentacao(rs));
+                }
+            }
+        }
+
+        return movimentacoes;
+    }
+
+    private Movimentacao mapearMovimentacao(ResultSet rs) throws SQLException {
+
+        return new Movimentacao(
+                rs.getInt("id"),
+                rs.getInt("conta_origem_id"),
+                rs.getInt("conta_destino_id"),
+                rs.getBigDecimal("valor"),
+                rs.getString("tipo"),
+                rs.getTimestamp("data_hora").toLocalDateTime(),
+                rs.getString("observacao")
+        );
     }
 }
