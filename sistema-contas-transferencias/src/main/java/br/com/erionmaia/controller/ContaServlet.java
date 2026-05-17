a73a78d48e09be70ca61b26c63359c38597f38de
Erion Schlenger de Paiva Maia <erion@riseio.com>
2026-05-16 22:17:56 -0300

    feat: cria servlet para fluxo de contas.

---
+++ sistema-contas-transferencias/src/main/java/br/com/erionmaia/controller/ContaServlet.java
@@ -0,0 +1,62 @@
+package br.com.erionmaia.controller;
+
+import br.com.erionmaia.model.Conta;
+import br.com.erionmaia.service.ContaService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+@WebServlet("/contas")
+public class ContaServlet extends HttpServlet {
+
+    private final ContaService contaService = new ContaService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            req.setAttribute("contas", contaService.listar());
+
+            req.getRequestDispatcher(
+                    "/WEB-INF/views/contas/listar.jsp")
+                    .forward(req, resp);
+        } catch (SQLException e) {
+            throw new ServletException("Erro ao listar contas.", e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            String.nomeTitular = req.getParameter("nomeTitualar");
+            String.numeroConta = req.getParameter("numeroConta");
+            BigDecimal saldo = new BigDecimal(req.getParameter("saldo"));
+            String status =  req.getParameter("status");
+
+            Conta conta = new Conta(
+                    null,
+                    nomeTitular,
+                    numeroConta,
+                    saldo,
+                    status,
+                    null
+            );
+
+            contaService.criarConta(conta);
+
+            resp.sendRedirect(req.getContextPath() + "/contas");
+        } catch (Exception e) {
+            req.setAttribute("erro", e.getMessage());
+
+            req.getRequestDispatcher("/WEB-INF/views/contas/formulario.jsp")
+                    .forward(req, resp);
+        }
+    }
+}
